56ede8f81bde70bf36fdc9a66f5ad1b4c93dc7e4
SusumiManage <63439713+SusumiManage@users.noreply.github.com>
2026-02-11 13:39:41 +0000

    Add files via upload
    Add frontend source code

---
+++ frontend/karma.conf.js
@@ -0,0 +1,29 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false
+    },
+    reporters: ['progress', 'kjhtml', 'coverage'],
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    restartOnFileChange: false
+  });
+};
